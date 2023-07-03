795febcc23e61099c2ee5b914df7819a088dfe8b
pSeUdAiSZ <98685805+pSeUdAiSZ@users.noreply.github.com>
2023-07-03 21:26:48 +0300

    Delete salonmanager directory

--- salonmanager/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Appointment,Service,StaffMember,Branch,Customer
-admin.site.register(Appointment)
-admin.site.register(Service)
-admin.site.register(StaffMember)
-admin.site.register(Branch)
-admin.site.register(Customer)
